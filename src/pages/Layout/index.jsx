bdb3dea721c85415fc58b08013e712e2971eb209
Kaozhi Chen <kylerchen16@gmail.com>
2024-04-20 16:22:28 -0400

    Highlighting menu based on route

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -6,7 +6,7 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons';
 import './index.scss';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 const { Header, Sider } = Layout;
 
@@ -33,6 +33,8 @@ const GeekLayout = () => {
   const onMenuClick = (route) => {
     navigate(route.key);
   };
+  const location = useLocation();
+  const selectedKey = location.pathname;
   return (
     <Layout>
       <Header className='header'>
@@ -51,7 +53,7 @@ const GeekLayout = () => {
           <Menu
             mode='inline'
             theme='dark'
-            defaultSelectedKeys={['1']}
+            selectedKeys={selectedKey}
             items={items}
             style={{ height: '100%', borderRight: 0 }}
             onClick={onMenuClick}
