91bb5d07234c90795b7635b628ca1eae3abd9d3d
Kaozhi Chen <kylerchen16@gmail.com>
2024-04-20 15:59:00 -0400

    home, article, and publish pages

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Layout from '@/pages/Layout';
 import AuthRoute from '@/components/AuthRoute';
+import Home from '@/pages/Home';
+import Article from '@/pages/Article';
+import Publish from '@/pages/Publish';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +15,20 @@ const router = createBrowserRouter([
         <Layout />
       </AuthRoute>
     ),
+    children: [
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'article',
+        element: <Article />,
+      },
+      {
+        path: 'publish',
+        element: <Publish />,
+      },
+    ],
   },
   {
     path: '/login',
