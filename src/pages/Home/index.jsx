017b5b2c33b7745865f7bf8841d9cee47370119e
Kaozhi Chen <kylerchen16@gmail.com>
2024-04-24 14:00:56 -0400

    ECharts

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as echarts from 'echarts';
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    //确保dom可用
+    const chartDom = document.getElementById('main');
+    const myChart = echarts.init(chartDom);
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar',
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }, []);
+  return (
+    <div>
+      <div id='main' style={{ width: '500px', height: '400px' }}></div>
+    </div>
+  );
 };
 
 export default Home;
