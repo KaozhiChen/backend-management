47145a16c1f3f8b8b1a7789ede0636b0cfd1b52e
Kaozhi Chen <kylerchen16@gmail.com>
2024-05-02 12:48:49 -0400

    route laze loading

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,9 +3,15 @@ import { createBrowserRouter } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Layout from '@/pages/Layout';
 import AuthRoute from '@/components/AuthRoute';
-import Home from '@/pages/Home';
-import Article from '@/pages/Article';
-import Publish from '@/pages/Publish';
+// import Home from '@/pages/Home';
+// import Article from '@/pages/Article';
+// import Publish from '@/pages/Publish';
+import { Suspense, lazy } from 'react';
+
+//lazy import
+const Home = lazy(() => import('@/pages/Home'));
+const Article = lazy(() => import('@/pages/Article'));
+const Publish = lazy(() => import('@/pages/Publish'));
 
 const router = createBrowserRouter([
   {
@@ -17,16 +23,28 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: 'home',
-        element: <Home />,
+        index: true,
+        element: (
+          <Suspense fallback={'Loading'}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: 'article',
-        element: <Article />,
+        element: (
+          <Suspense fallback={'Loading'}>
+            <Article />
+          </Suspense>
+        ),
       },
       {
         path: 'publish',
-        element: <Publish />,
+        element: (
+          <Suspense fallback={'Loading'}>
+            <Publish />
+          </Suspense>
+        ),
       },
     ],
   },
